ea6a8935b9a96092acd7dc8610dd0d29baba5310
Jibril Gupa <gupajibril@gmail.com>
2024-01-04 10:10:17 +0100

    add handling of bifrost and bifrost-polkadot

--- src/service_args.py
+++ src/service_args.py
@@ -123,6 +123,8 @@ class ServiceArgs():
             self.__origintrail()
         elif self.chain_name == 'asset-hub-rococo':
             self.__asset_hub_rococo()
+        elif self.chain_name in ['bifrost', 'bifrost-polkadot']:
+            self.__bifrost()
 
     def __peregrine(self):
         self.__replace_chain_name(Path(utils.HOME_PATH, 'dev-specs/kilt-parachain/peregrine-kilt.json'), 0)
@@ -240,5 +242,18 @@ class ServiceArgs():
     def __asset_hub_rococo(self):
         chain_json_url = 'https://raw.githubusercontent.com/paritytech/polkadot-sdk/master/cumulus/parachains/chain-specs/asset-hub-rococo.json'
         chain_json_path = f'{utils.CHAIN_SPEC_PATH}/{self.chain_name}.json'
+        utils.download_chain_spec(chain_json_url, f'{self.chain_name}.json')
+        self.__replace_chain_name(chain_json_path, 0)
+
+    def __bifrost(self):
+        if self.chain_name == 'bifrost':
+            chain_json_url = 'https://raw.githubusercontent.com/bifrost-finance/bifrost/develop/node/service/res/bifrost-kusama.json'
+        elif self.chain_name == 'bifrost-polkadot':
+            chain_json_url = 'https://raw.githubusercontent.com/bifrost-finance/bifrost/develop/node/service/res/bifrost-polkadot.json'
+        else:
+            raise ValueError("Unsupported chain name.")
+
+        chain_json_path = f'{utils.CHAIN_SPEC_PATH}/{self.chain_name}.json'
+
         utils.download_chain_spec(chain_json_url, f'{self.chain_name}.json')
         self.__replace_chain_name(chain_json_path, 0)
