0e97d3a8187123c53d653bf2d3a2344a9291f0ff
kaioh33 <gupajibril@gmail.com>
2024-08-07 15:29:39 +0100

    allow litentry docker image

--- src/docker.py
+++ src/docker.py
@@ -71,6 +71,8 @@ class Docker():
             self.__extract_from_docker('opentensor/subtensor', 'usr/local/bin/node-subtensor')
         elif self.chain_name in ['peaq', 'krest']:
             self.__extract_from_docker('peaq/parachain', 'usr/local/bin/peaq-node')
+        elif self.chain_name == 'litentry':
+            self.__extract_from_docker('litentry/litentry-parachain', '/usr/local/bin/litentry-collator')
         else:
             raise ValueError(f"{self.chain_name} is not a supported chain using Docker!")
 
