36990bb514aade9582c0a31d71ce948a6638084c
A Ahmad <57046949+abbaahmad@users.noreply.github.com>
2024-10-29 09:04:23 +0100

    Validate chain spec files on config change (#84)
    * Validate chain spec files on config-change

--- src/charm.py
+++ src/charm.py
@@ -128,15 +128,24 @@ class PolkadotCharm(ops.CharmBase):
             self._stored.service_args = self.config.get('service-args')
 
         if self._stored.chain_spec_url != self.config.get('chain-spec-url'):
-            self.unit.status = ops.MaintenanceStatus("Updating chain spec")
-            utils.update_service_args(service_args_obj.service_args_string)
-            self._stored.chain_spec_url = self.config.get('chain-spec-url')
+            try:
+                self.unit.status = ops.MaintenanceStatus("Updating chain spec")
+                utils.update_service_args(service_args_obj.service_args_string)
+                self._stored.chain_spec_url = self.config.get('chain-spec-url')
+            except ValueError as e:
+                self.unit.status = ops.BlockedStatus(str(e))
+                event.defer()
+                return
 
         if self._stored.local_relaychain_spec_url != self.config.get('local-relaychain-spec-url'):
-            self.unit.status = ops.MaintenanceStatus("Updating relaychain spec")
-            utils.update_service_args(service_args_obj.service_args_string)
-            self._stored.local_relaychain_spec_url = self.config.get('local-relaychain-spec-url')
-
+            try:
+                self.unit.status = ops.MaintenanceStatus("Updating relaychain spec")
+                utils.update_service_args(service_args_obj.service_args_string)
+                self._stored.local_relaychain_spec_url = self.config.get('local-relaychain-spec-url')
+            except ValueError as e:
+                self.unit.status = ops.BlockedStatus(str(e))
+                event.defer()
+                return
         if self._stored.wasm_runtime_url != self.config.get('wasm-runtime-url'):
             self.unit.status = ops.MaintenanceStatus("Updating wasm runtime")
             utils.download_wasm_runtime(self.config.get('wasm-runtime-url'))
