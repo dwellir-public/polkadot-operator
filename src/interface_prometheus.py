0a1a12e799baeef15dd54bb4a73f215ec23dc024
Joakim Nyman <joakim.nyman86@gmail.com>
2026-03-18 17:32:35 +0100

    Support multiple units to prometheus and added labels.

--- src/interface_prometheus.py
+++ src/interface_prometheus.py
@@ -1,69 +1,101 @@
 #!/usr/bin/python3
-"""prometheus scrape interface (provides side)."""
+"""Prometheus manual jobs provider."""
 
 import hashlib
 import json
+import socket
 
 from ops.framework import Object
 
 
 class PrometheusProvider(Object):
-    """Prometheus  provider interface."""
+    """Publish manual scrape jobs to prometheus2."""
 
-    def __init__(self, charm, relation_name, port, path, **job_data):
+    def __init__(self, charm, relation_name, port, path, job_name=None, **job_data):
         """
-        Attaches a dictionary containing job data. If you wish to overwrite
-        individual fields like "honor_timestamps", you can add your own value
-        for it as a keyword argument. Just make sure all keyword arguments are
-        valid keys in the prometheus configuration files.
+        Configure a prometheus-manual job publisher.
+
+        If you wish to overwrite individual fields like
+        "honor_timestamps", you can add your own value for it as a keyword
+        argument. Just make sure all keyword arguments are valid keys in the
+        Prometheus configuration files.
         """
         super().__init__(charm, relation_name)
         self._relation_name = relation_name
-        request_id = hashlib.sha256(
+        self._job_name = job_name or relation_name
+        self._path = path
+        self._port = str(port)
+        self._job_data = job_data
+        self._request_id = hashlib.sha1(
             f"{self.model.uuid}:{relation_name}:{self.model.unit.name}".encode()
-        ).hexdigest()
-        self.job = {
-            "job_name": f"{relation_name}",
-            "job_data": {
-                "honor_timestamps": True,
-                "scrape_interval": "15s",
-                "scrape_timeout": "15s",
-                "metrics_path": path,
-                "scheme": "http",
-                "follow_redirects": True,
-                "enable_http2": True,
-                **job_data,
-                },
-            "request_id": request_id,
-            "port": str(port),
-        }
-        # NOTE
-        # request_id must be unique for every relation and unit in the current model.
-        # job_name in Prometheus will become (job_name + "-" + request_id). Note the dash between those two variables that will be automatically added by the Prometheus charm.
-        # Example of a resulting job_name in Prometheus: polkadot-prometheus-<request-id>
+        ).hexdigest()[:8]
 
         self.framework.observe(
             charm.on[relation_name].relation_joined, self._on_relation_joined
         )
 
     def _on_relation_joined(self, event):
-        """We use this event for passing on hostname and port and metrics_path.
+        """Publish the manual scrape job when related."""
+        self.set_job(event)
 
-        :param event:
-        :return:
-        """
+    def _job(self, bind_address):
+        """Build the manual scrape job payload."""
+        job_name_prefix = "-".join(
+            part
+            for part in (
+                self.model.name,
+                self.model.app.name,
+                self.model.unit.name.replace("/", "-"),
+                self._job_name,
+            )
+            if part
+        )
+        labels = {
+            "juju_model": self.model.name,
+            "juju_model_uuid": self.model.uuid,
+            "juju_application": self.model.app.name,
+            "juju_unit": self.model.unit.name,
+            "hostname": socket.gethostname(),
+        }
+
+        # prometheus2 strips the last five dash-delimited segments when
+        # deduplicating manual jobs, so keep the meaningful identifier before
+        # this short suffix.
+        dedupe_suffix = "x-x-x-x"
+        return {
+            "job_name": f"{job_name_prefix}-{dedupe_suffix}",
+            "job_data": {
+                "honor_timestamps": True,
+                "scrape_interval": "15s",
+                "scrape_timeout": "15s",
+                "metrics_path": self._path,
+                "scheme": "http",
+                "follow_redirects": True,
+                "enable_http2": True,
+                "static_configs": [
+                    {
+                        "targets": [f"{bind_address}:{self._port}"],
+                        "labels": labels,
+                    }
+                ],
+                **self._job_data,
+            },
+            "request_id": self._request_id,
+            "port": self._port,
+        }
+
+    def set_job(self, event=None):
+        """Publish the current scrape job to all related consumers."""
         bind_address = getattr(
             self.model.get_binding(self._relation_name).network,
             "bind_address",
             None,
         )
         if not bind_address:
-            event.defer()
+            if event:
+                event.defer()
             return
 
-        self.job["job_data"]["static_configs"] = [
-            {"targets": [f"{bind_address}:{self.job['port']}"]}
-        ]
-        event.relation.data[self.model.unit][
-            f'request_{self.job["request_id"]}'
-        ] = json.dumps(self.job, sort_keys=True)
+        job = json.dumps(self._job(str(bind_address)), sort_keys=True)
+        for relation in self.model.relations[self._relation_name]:
+            relation.data[self.model.unit][f"request_{self._request_id}"] = job
