ade2aefdec410eef2644f6081837b7972177b080
Joakim Nyman <joakim.nyman86@gmail.com>
2026-03-18 10:26:39 +0100

    Publish relation addresses from bind_address instead of ingress-address

--- src/interface_prometheus.py
+++ src/interface_prometheus.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """prometheus scrape interface (provides side)."""
 
-from ops.framework import Object
+import hashlib
 import json
-import socket
-from uuid import uuid4
+
+from ops.framework import Object
+
 
 class PrometheusProvider(Object):
     """Prometheus  provider interface."""
@@ -17,8 +18,12 @@ class PrometheusProvider(Object):
         valid keys in the prometheus configuration files.
         """
         super().__init__(charm, relation_name)
+        self._relation_name = relation_name
+        request_id = hashlib.sha256(
+            f"{self.model.uuid}:{relation_name}:{self.model.unit.name}".encode()
+        ).hexdigest()
         self.job = {
-            "job_name": f'{socket.gethostname()}_{self.model.name}_{relation_name}_uuid-{uuid4()}',
+            "job_name": f"{relation_name}",
             "job_data": {
                 "honor_timestamps": True,
                 "scrape_interval": "15s",
@@ -29,17 +34,14 @@ class PrometheusProvider(Object):
                 "enable_http2": True,
                 **job_data,
                 },
-            "request_id": str(uuid4())[-6:],
+            "request_id": request_id,
             "port": str(port),
         }
         # NOTE
-        # request_id must be unique for every relation and unit in the current model, hence the use of UUID.
+        # request_id must be unique for every relation and unit in the current model.
         # job_name in Prometheus will become (job_name + "-" + request_id). Note the dash between those two variables that will be automatically added by the Prometheus charm.
-        # Example of a resulting job_name in Prometheus: dwellir-westend-rpc-1_juju-a4c6ea-0_node-prometheus-d95f1796-f317-457a-b730-c3de4f72b8ff-68916a
-        #
-        # Prometheus charm assumes job_name ends with an UUID so we need to have one there as well.
-        # See: https://git.launchpad.net/charm-prometheus2/commit/?id=26c4a20163a7d655bb1e0e5e925114e57bf16b4a
-        
+        # Example of a resulting job_name in Prometheus: polkadot-prometheus-<request-id>
+
         self.framework.observe(
             charm.on[relation_name].relation_joined, self._on_relation_joined
         )
@@ -50,7 +52,18 @@ class PrometheusProvider(Object):
         :param event:
         :return:
         """
-        ingress_address = event.relation.data.get(self.model.unit)['ingress-address']
-        if "static_configs" not in self.job["job_data"]:
-            self.job["job_data"]["static_configs"] = [{"targets": [f"{ingress_address}:{self.job['port']}"]}]
-        event.relation.data[self.model.unit][f'request_{self.job["request_id"]}'] = json.dumps(self.job, sort_keys=True)
+        bind_address = getattr(
+            self.model.get_binding(self._relation_name).network,
+            "bind_address",
+            None,
+        )
+        if not bind_address:
+            event.defer()
+            return
+
+        self.job["job_data"]["static_configs"] = [
+            {"targets": [f"{bind_address}:{self.job['port']}"]}
+        ]
+        event.relation.data[self.model.unit][
+            f'request_{self.job["request_id"]}'
+        ] = json.dumps(self.job, sort_keys=True)
